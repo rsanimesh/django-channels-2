1b77752057c358f22de9a4890df2a05ac4d7b99b
Rahul Kumar Sinha <rsanimesh@gmail.com>
2019-07-27 15:00:30 +0530

    add default view

--- chat/views.py
+++ chat/views.py
@@ -12,3 +12,6 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+def default(request):
+    return "Welcome to Django Channnels 2 Application."
